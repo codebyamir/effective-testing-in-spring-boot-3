832e7783fa17cfe31a88449d42d37380b494baaf
Amir Boroumand <amir@wireprivacy.org>
2024-04-07 21:25:54 -0400

    Updates

--- src/test/java/com/steelcityamir/commerce/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/steelcityamir/commerce/controller/ProductControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.steelcityamir.commerce.controller;
 
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,7 @@
+-- Products table
+DROP TABLE IF EXISTS products CASCADE;
 
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE products (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     created_on TIMESTAMP WITH TIME ZONE,
     product_name VARCHAR(255) NOT NULL,
@@ -9,4 +11,52 @@ CREATE TABLE IF NOT EXISTS products (
     units_in_stock INT
 );
 
-TRUNCATE TABLE products RESTART IDENTITY;
+-- Users Table
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash CHAR(64) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Carts Table
+DROP TABLE IF EXISTS carts CASCADE;
+
+CREATE TABLE IF NOT EXISTS carts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Cart_Items Table
+
+DROP TABLE IF EXISTS cart_items CASCADE;
+
+CREATE TABLE cart_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cart_id INT,
+    product_id INT,
+    quantity INT,
+  --  added_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (cart_id) REFERENCES carts(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+-- Orders Table
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    cart_id INT,
+    total_price DECIMAL(10, 2),
+    status VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (cart_id) REFERENCES carts(id)
+);
